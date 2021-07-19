163dd1b54af47a2d182ab4038ab79302cfd63d2b
InTheMainframe <80524213+InTheMainframe@users.noreply.github.com>
2021-07-19 23:19:29 +1000

    Refactored 👍

--- index.js
+++ index.js
@@ -1,48 +1,54 @@
-const Discord = require('discord.js'); // for controlling the discord bot
-const request = require('request'); // for loading petittube
-const cheerio = require('cheerio'); // for parsing the html
-const fs = require('fs'); // just to read the token.txt file
+const Discord = require("discord.js"),
+    client = new Discord.Client(),
+    request = require("request"),
+    cheerio = require("cheerio"),
+    fs = require("fs"),
+    token = fs.readFileSync("token.txt", "utf8").trim();
 
-const client = new Discord.Client();
-const token = fs.readFileSync('token.txt', 'utf8').trim();
-
-console.log('Connecting to Discord...');
-var connectStart = Date.now();
-client.on('ready', () => {
-    var connectTime = Date.now() - connectStart;
-    console.log(`Connected in ${connectTime}ms - Logged in as: ${client.user.tag}`);
+console.log("Connecting to Discord...");
+let start = Date.now();
+client.on("ready", () => {
+    console.log(`Connected in ${Date.now() - start}ms - Logged in as: ${client.user.tag}`);
 });
 
-client.on('message', async (message) => {
-    // check if the bot is mentioned
-    if (message.content.startsWith(`<@${client.user.id}>`) || message.content.startsWith(`<@!${client.user.id}>`)) {
-        message.channel.send({
-            embed: {
-                description: "⏳ Loading... ⏳",
-                color: '#ff0000',
-            },
-        }).then((sentMessage) => {
-            var requestStart = Date.now();
-            request({
-                url: 'http://petittube.com/',
-                // deliberately using http instead of https for fastest response
-                headers: {
-                    'User-Agent': 'Mozilla/5.0 (compatible; PetitTubeBot/1.0; +https://github.com/InTheMainframe/PetitTubeBot)',
+client.on("message", async msg => {
+    let x = client.user.id,
+        mentioned = msg.content.startsWith(`<@${x}>`) || msg.content.startsWith(`<@!${x}>`);
+    if (mentioned) {
+        msg.channel
+            .send({
+                embed: {
+                    description: "⏳ Loading... ⏳",
+                    color: "#ff0000",
                 },
-            }, (error, response, html) => {
-                var responseTime = Date.now() - requestStart;
-                // an expected iframe tag would look like:
-                // <iframe width="630" height="473" src="https://www.youtube.com/embed/dQw4w9WgXcQ?version=3&f=videos&app=youtube_gdata&autoplay=1" frameborder="0" allowfullscreen></iframe>
-                // with the youtube embed link being the src attribute, and the example video id being dQw4w9WgXcQ
-                var $ = cheerio.load(html); // the whole page as html
-                var embedLink = $('iframe').attr('src'); // the embed link (containing the video id)
-                var prefix = 'https://www.youtube.com/embed/';
-                var suffix = '?version=3&f=videos&app=youtube_gdata&autoplay=1'; // the useless parts of the embed link to ignore
-                var videoID = embedLink.substring(prefix.length).slice(0, -suffix.length); // leaves us with the video id
-                var videoLink = 'https://www.youtube.com/watch?v=' + videoID; // make a normal youtube link we can send
-                sentMessage.edit(`${videoLink} - Loaded in ${responseTime}ms`).catch();
-            });
-        }).catch();
+            })
+            .then(sentMsg => {
+                let t = Date.now();
+                request(
+                    {
+                        url: "http://petittube.com/",
+                        headers: {
+                            "User-Agent": "Mozilla/5.0 (compatible; PetitTubeBot/1.0; +https://bit.ly/3kzriOv)",
+                        },
+                    },
+                    (err, resp, html) => {
+                        if (err) return msg.reply(`Uh oh, a request error D:\n\`${err}\``).catch();
+                        let r = Date.now() - t,
+                            idPref = "https://www.youtube.com/embed/",
+                            idSuff = "?version=3&f=videos&app=youtube_gdata&autoplay=1",
+                            link =
+                                "https://www.youtube.com/watch?v=" +
+                                cheerio
+                                    .load(html)("iframe")
+                                    .attr("src")
+                                    .substring(idPref.length)
+                                    .slice(0, -idSuff.length);
+
+                        sentMsg.edit(`${link} - Loaded in ${r}ms`).catch();
+                    }
+                );
+            })
+            .catch();
     }
 });
 
