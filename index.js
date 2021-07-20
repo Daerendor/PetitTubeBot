5fa551cdb585254f136b7ca247b1f54ccc75aa50
Daerendor <54585742+Daerendor@users.noreply.github.com>
2021-07-20 10:09:04 +0200

    Fix: Spare time sending requests

--- index.js
+++ index.js
@@ -8,47 +8,59 @@ const Discord = require("discord.js"),
 console.log("Connecting to Discord...");
 let start = Date.now();
 client.on("ready", () => {
-    console.log(`Connected in ${Date.now() - start}ms - Logged in as: ${client.user.tag}`);
+    console.log(
+        `Connected in ${Date.now() - start}ms - Logged in as: ${
+            client.user.tag
+        }`
+    );
 });
 
-client.on("message", async msg => {
+client.on("message", async (msg) => {
     let x = client.user.id,
-        mentioned = msg.content.startsWith(`<@${x}>`) || msg.content.startsWith(`<@!${x}>`);
+        mentioned =
+            msg.content.startsWith(`<@${x}>`) ||
+            msg.content.startsWith(`<@!${x}>`);
     if (mentioned) {
-        msg.channel
-            .send({
+        let startDate = Date.now();
+
+        Promise.all([
+            msg.channel.send({
                 embed: {
                     description: "⏳ Loading... ⏳",
                     color: "#ff0000",
                 },
-            })
-            .then(sentMsg => {
-                let t = Date.now();
+            }),
+            new Promise((resolve, reject) => {
                 request(
                     {
                         url: "http://petittube.com/",
                         headers: {
-                            "User-Agent": "Mozilla/5.0 (compatible; PetitTubeBot/1.0; +http://github.com/jlij/PetitTubeBot)",
+                            "User-Agent":
+                                "Mozilla/5.0 (compatible; PetitTubeBot/1.0; +http://github.com/jlij/PetitTubeBot)",
                         },
                     },
-                    (err, resp, html) => {
-                        if (err) return msg.reply(`Uh oh, a request error D:\n\`${err}\``).catch();
-                        let r = Date.now() - t,
-                            idPref = "https://www.youtube.com/embed/",
-                            idSuff = "?version=3&f=videos&app=youtube_gdata&autoplay=1",
-                            link =
-                                "https://www.youtube.com/watch?v=" +
-                                cheerio
-                                    .load(html)("iframe")
-                                    .attr("src")
-                                    .substring(idPref.length)
-                                    .slice(0, -idSuff.length);
-
-                        sentMsg.edit(`${link} - Loaded in ${r}ms`).catch();
+                    (err, res) => {
+                        if (err) return reject(err);
+                        return resolve(res);
                     }
                 );
+            }),
+        ])
+            .then(([sentMsg, {html: body}]) => {
+                let responseTime = Date.now() - startDate,
+                    idPref = "https://www.youtube.com/embed/",
+                    idSuff = "?version=3&f=videos&app=youtube_gdata&autoplay=1",
+                    link =
+                        "https://www.youtube.com/watch?v=" +
+                        cheerio
+                            .load(html)("iframe")
+                            .attr("src")
+                            .substring(idPref.length)
+                            .slice(0, -idSuff.length);
+
+                sentMsg.edit(`${link} - Loaded in ${responseTime}ms`).catch();
             })
-            .catch();
+            .catch(/* It broke somewhere */);
     }
 });
 
